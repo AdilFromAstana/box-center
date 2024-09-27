58beaa9dec85047513ce7292554c2fb78fcf526c
adilfirstus@gmail.com <adilfirstus@gmail.com>
2024-09-27 18:50:42 +0500

    fix

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -39,9 +39,7 @@ const EventsList = () => {
 
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth <= 480) {
-                setItemsPerPage(1);
-            } else if (window.innerWidth <= 720) {
+            if (window.innerWidth <= 720) {
                 setItemsPerPage(2);
             } else if (window.innerWidth <= 1024) {
                 setItemsPerPage(3);
@@ -86,7 +84,7 @@ const EventsList = () => {
                     {events.map((event, index) => (
                         <div key={index} className="flex-shrink-0 px-2" style={{ width: `${100 / itemsPerPage}%` }}>
                             <div className="flex flex-col justify-between h-full">
-                                <div className="relative">
+                                <div className="relative flex flex-col justify-between">
                                     <div className="aspect-[9/16]">
                                         <img
                                             alt={event.text}
@@ -98,40 +96,42 @@ const EventsList = () => {
                                         {event.minAge}+
                                     </div>
                                 </div>
-                                <div className="text-center text-sm text-[#006D56]">{event.text}</div>
-                                <div className="flex items-center flex-row gap-4">
-                                    <svg
-                                        width="20"
-                                        height="20"
-                                        viewBox="0 0 25 25"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <rect
-                                            x="0.5"
-                                            y="0.219727"
-                                            width="24"
-                                            height="24"
-                                            fill="url(#pattern0_29_290)"
-                                        />
-                                        <defs>
-                                            <pattern
-                                                id="pattern0_29_290"
-                                                patternContentUnits="objectBoundingBox"
-                                                width="1"
-                                                height="1"
-                                            >
-                                                <use xlinkHref="#image0_29_290" transform="scale(0.0416667)" />
-                                            </pattern>
-                                            <image
-                                                id="image0_29_290"
+                                <div className="py-2 h-full flex flex-col items-center justify-between">
+                                    <div className="text-center text-sm text-[#006D56]">{event.text}</div>
+                                    <div className="flex items-center flex-row gap-4">
+                                        <svg
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 25 25"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <rect
+                                                x="0.5"
+                                                y="0.219727"
                                                 width="24"
                                                 height="24"
-                                                xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAIFSURBVHgBzVS/a1NRFP7OzRMxo0l0LE5C3XRxsSCK4NLB5r03FNGhiNi+gP9AB0cdlBhDkIIV6ZCkOnQRoi66i4sBJ8moSTqmlCbv9OS+l/SlL31pS1L6weWed+53z3fPDx5hCChjVcCYYkIR2dIyopCxnhHDBqHG2dKd/cdq6CXGBe6wrWTHCCifiwO45L3CfKSYru7FR1ou3aYYrQnhR5SAcG8Id164X4W73vO7xD+RLb81tArTE7fDD/uqMbrpm1XxF6IEhJvwzWaQK/5VEfcENPLlX/1Dx64ghlUmzgf9QzPImF+6wQhU4Xxpj+tYgVc4VmSQ46AXs5+BcqwCxgj2977AqFofFVK26wMCo2p9ZPg9MEYSn5rnqE3fGLwgX2nfuy5NXWGDb9GOWmHl6vEkV6U5V5wPxZhkkxUmjFCJaMleG0j5jLtwqksUbvKSeQnx7Ya2W2eTyJX/YnHuCpLNP6inktqfqjfQSFzGm4+/h/IDCPeAcB+t+DW9urZMkVLGY2xenIJSd/USW/vkLMSPSmecOLhEjn0P3K56LGMa5/9toJl6IK/bEM+0z6rKv2AWifp7yWZ2gP+6+CkYbuJj6gmfbInMZXDsu8fqzAjjhWqr5y7wSsoyo/254jscEmEBxgfEW72xq+FlectdnCvImNZkTD/jOJh8iQj/xy4iMbvbLtAk3o8mVbmqAAAAAElFTkSuQmCC"
+                                                fill="url(#pattern0_29_290)"
                                             />
-                                        </defs>
-                                    </svg>
-                                    <span className="font-semibold">{event.date}</span>
+                                            <defs>
+                                                <pattern
+                                                    id="pattern0_29_290"
+                                                    patternContentUnits="objectBoundingBox"
+                                                    width="1"
+                                                    height="1"
+                                                >
+                                                    <use xlinkHref="#image0_29_290" transform="scale(0.0416667)" />
+                                                </pattern>
+                                                <image
+                                                    id="image0_29_290"
+                                                    width="24"
+                                                    height="24"
+                                                    xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAIFSURBVHgBzVS/a1NRFP7OzRMxo0l0LE5C3XRxsSCK4NLB5r03FNGhiNi+gP9AB0cdlBhDkIIV6ZCkOnQRoi66i4sBJ8moSTqmlCbv9OS+l/SlL31pS1L6weWed+53z3fPDx5hCChjVcCYYkIR2dIyopCxnhHDBqHG2dKd/cdq6CXGBe6wrWTHCCifiwO45L3CfKSYru7FR1ou3aYYrQnhR5SAcG8Id164X4W73vO7xD+RLb81tArTE7fDD/uqMbrpm1XxF6IEhJvwzWaQK/5VEfcENPLlX/1Dx64ghlUmzgf9QzPImF+6wQhU4Xxpj+tYgVc4VmSQ46AXs5+BcqwCxgj2977AqFofFVK26wMCo2p9ZPg9MEYSn5rnqE3fGLwgX2nfuy5NXWGDb9GOWmHl6vEkV6U5V5wPxZhkkxUmjFCJaMleG0j5jLtwqksUbvKSeQnx7Ya2W2eTyJX/YnHuCpLNP6inktqfqjfQSFzGm4+/h/IDCPeAcB+t+DW9urZMkVLGY2xenIJSd/USW/vkLMSPSmecOLhEjn0P3K56LGMa5/9toJl6IK/bEM+0z6rKv2AWifp7yWZ2gP+6+CkYbuJj6gmfbInMZXDsu8fqzAjjhWqr5y7wSsoyo/254jscEmEBxgfEW72xq+FlectdnCvImNZkTD/jOJh8iQj/xy4iMbvbLtAk3o8mVbmqAAAAAElFTkSuQmCC"
+                                                />
+                                            </defs>
+                                        </svg>
+                                        <span className="font-semibold text-sm">{event.date}</span>
+                                    </div>
                                 </div>
                                 <a href={'/e/' + event.id} className="bg-[#006D56] flex justify-center">
                                     <button className="text-white">Купить билет</button>
